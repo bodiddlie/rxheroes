338b25f64ac3c284a651ccc2dc2f1cf8a2cebb93
Nick Klepinger <bodiddlie@gmail.com>
2016-06-15 21:54:00 -0700

    upgraded to rc2 started adding dashboard stuff

--- app/components/dashboard/dashboard.component.ts
+++ app/components/dashboard/dashboard.component.ts
@@ -1,11 +1,35 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Observable} from 'rxjs/Observable';
+import {Store} from '@ngrx/store';
+
+import {AppState} from '../../reducers';
+import {HeroActions} from '../../actions';
 
 @Component({
     selector: 'rx-dashboard',
     template: `
-        <h1>Dashboard</h1>
-    `
+        <h3>Top Heroes</h3>
+        <div class="grid grid-pad">
+            <div *ngFor="let hero of (heroes | async)" class="col-1-4">
+                <div class="module hero">
+                    <h4>{{hero.name}}</h4>
+                </div>
+            </div>
+        </div>
+    `,
+    styles: [require('./dashboard.component.css')]
 })
-export class Dashboard {
+export class Dashboard implements OnInit {
+    heroes: Observable<any>;
 
+    constructor(
+        private store: Store<AppState>,
+        private heroActions: HeroActions
+    ) {
+        this.heroes = store.select('heroes');
+    }
+
+    ngOnInit() {
+        this.store.dispatch(this.heroActions.loadHeroes());
+    }
 }
