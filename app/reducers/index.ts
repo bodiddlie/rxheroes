e9f9052656fc415667a42da3e1b4b975bf22ad85
bodiddlie <bodiddlie@gmail.com>
2016-06-21 11:53:02 -0700

    added info about storeLogger

--- app/reducers/index.ts
+++ app/reducers/index.ts
@@ -1,6 +1,6 @@
 import '@ngrx/core/add/operator/select';
 import {compose} from '@ngrx/core/compose';
-import {storeLogger} from 'ngrx-store-logger';
+//import {storeLogger} from 'ngrx-store-logger';
 import {combineReducers} from '@ngrx/store';
 
 import heroListReducer, * as fromHeroList from './hero-list';
@@ -11,6 +11,12 @@ export interface AppState {
     hero: fromHero.HeroState;
 };
 
+//uncomment the storeLogger import and this line
+//and comment out the other export default line to turn on
+//the store logger to see the actions as they flow through the store
+//turned this off by default as i found the logger kinda noisy
+
+//export default compose(storeLogger(), combineReducers)({
 export default compose(combineReducers)({
     heroes: heroListReducer,
     hero: heroReducer
